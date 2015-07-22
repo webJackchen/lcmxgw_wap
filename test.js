6e85e84295fb59c57f123a1c0094cf057925c0aa
chenzhongfei <zf.chen@dsecet.com>
2015-07-22 11:22:21 +0800

    ...

--- test.js
+++ test.js
@@ -2,12 +2,14 @@
     var distY = 0;
     var dist = 0;
     var i = 0;
-    addEvent(document,"touchstart",function(ev){
+    addEvent(document.getElementsByTagName('body')[0],"touchstart",function(ev){
+        ev.preventDefault();
         var touch = ev.changedTouches[0];
         distY = touch.pageY;
         console.log(i++);
     });
-    addEvent(document,"touchmove",function(ev){
+    addEvent(document.getElementsByTagName('body')[0],"touchmove",function(ev){
+        ev.preventDefault();
         var touch = ev.changedTouches[0];
         dist = touch.pageY - distY;
         if( dist < 0 && Math.abs(dist) > 20 ){
@@ -16,7 +18,8 @@
             document.write("down");
         }
     })
-    addEvent(document,"touchend",function(ev){
+    addEvent(document.getElementsByTagName('body')[0],"touchend",function(ev){
+        ev.preventDefault();
         dist = 0;
     })
     function addEvent(obj,event,fn){
